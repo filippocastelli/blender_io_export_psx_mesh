971b8fd1e93465ce90ad00a4be466c756f7b178a
Filippo Maria Castelli <filippocastelli42@gmail.com>
2022-08-19 00:05:26 +0200

    simplify code, properly unregister

--- 3dcam_engine_helper_addon/__init__.py
+++ 3dcam_engine_helper_addon/__init__.py
@@ -1,8 +1,6 @@
 import bpy
 from .engine_helper import (
     ENGINEHELPER_PT_customPropsPanel,
-    selection_callback,
-    ENGINEHELPER_OT_copyCustomPropToSelection,
     Flags,
     Others
 )
@@ -20,56 +18,28 @@ bl_info = {
     "description": "Set/Unset/Copy flags easily",
 }
 
-
-def menu_func(self, context):
-    self.layout.operator(ENGINEHELPER_OT_copyCustomPropToSelection.bl_idname)
-
-
+# TODO: reimplement copy props functionality with new props
 def register():
-    # register the panel class
-    bpy.utils.register_class(Flags)
-    bpy.utils.register_class(Others)
-    bpy.utils.register_class(ENGINEHELPER_PT_customPropsPanel,
-)
+    classes = (
+        Flags, Others, ENGINEHELPER_PT_customPropsPanel
+    )
+    for _class in classes:
+        bpy.utils.register_class(_class)
+        
     bpy.types.Object.EH_Flags = bpy.props.PointerProperty(type=Flags)
     bpy.types.Object.EH_Others = bpy.props.PointerProperty(type=Others)
-    
-    #bpy.app.handlers.depsgraph_update_post.clear()
-    #bpy.app.handlers.depsgraph_update_post.append(selection_callback)
-    # copy helper
-    bpy.utils.register_class(ENGINEHELPER_OT_copyCustomPropToSelection)
-    bpy.types.VIEW3D_MT_object.append(menu_func)
-    # shortcut
-    wm = bpy.context.window_manager
-    km = wm.keyconfigs.addon.keymaps.new(name="Object Mode", space_type="EMPTY")
-    kmi = km.keymap_items.new(
-        ENGINEHELPER_OT_copyCustomPropToSelection.bl_idname, "P", "PRESS", ctrl=False, shift=True
-    )
-    # ~ kmi.properties.total = 4
-    addon_keymaps.append((km, kmi))
 
 
 def unregister():
     # unregister the panel class
-    bpy.utils.unregister_class(Flags)
-    bpy.utils.unregister_class(Others)
-    bpy.utils.unregister_class(ENGINEHELPER_PT_customPropsPanel,
-)
-    # unregister our components
-    try:
-        for key, value in bpy.samplePropertyGroups.items():
-            delattr(bpy.types.Object, key)
-            bpy.utils.unregister_class(value)
-    except UnboundLocalError:
-        pass
-    bpy.samplePropertyGroups = {}
-    # copy helper
-    bpy.utils.unregister_class(ENGINEHELPER_OT_copyCustomPropToSelection)
-    bpy.types.VIEW3D_MT_object.remove(menu_func)
-    # handle the keymap
-    for km, kmi in addon_keymaps:
-        km.keymap_items.remove(kmi)
-    addon_keymaps.clear()
+    classes = (
+        Flags, Others, ENGINEHELPER_PT_customPropsPanel
+    )
+    for _class in classes:
+        bpy.utils.register_class(_class)
+        
+    del bpy.types.Object.EH_Flags
+    del bpy.types.Object.EH_Others
 
 
 if __name__ == "__main__":
